17bd43261023ea3eaaf5d6b113f95baa39624f7d
Akhileshsh028 <akhileshsh028@outlook.com>
2017-03-23 12:17:55 +0000

    commit to Program as

---
+++ Anonymouss_Methods/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+delegate void Transformas (int x);                                  //Anonymous Methods
+namespace cSharpExamples
+{
+    class TestDelegate
+        {
+        static int num = 10;
+        public static void AddNum(int p)
+            {
+              num+= p;
+              Console.WriteLine("num method add :{0}", num);
+            }
+        public static void Multi(int p)
+            {
+               num*= p;
+            Console.WriteLine("num method add : {0}", num); 
+            }
+        public static int getnum()
+            {
+            return num;
+            }
+
+        }
+
+    class Program
+    {
+        static void Main()
+        { 
+        
+           Transformas sqrmulten= delegate(int x)
+           {
+            Console.WriteLine("Anonymous Method {0}",x); };
+
+           sqrmulten(10);
+            sqrmulten = new Transformas(AddNum());
+            sqrmulten(20);
+            sqrmulten = new Transformas(Multi());
+            Console.Read();
+        
+        }
+    }
+}
