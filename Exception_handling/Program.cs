3ac4356d66d2ca5dbc45863b139d724ae733ba5d
Akhilesh Kumar <akhileshsh026@hotmail.com>
2017-04-18 19:58:25 +0530

    exception handling throwing exception

--- Exception_handling/Program.cs
+++ Exception_handling/Program.cs
@@ -6,15 +6,53 @@ using System.Threading.Tasks;
 
 namespace Exception_handling
 {
+    class MyException : Exception
+    {
+        public MyException(string str)
+        {
+             Console.WriteLine("my Custom Exception from the class");
+        }
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
-                try
+            UserDefinedExceptions();
+            throwingExceptionprgra();
+
+        }
+
+        private static void UserDefinedExceptions()
+        {
+            try
             {
-                Console.WriteLine("My name is "); return;
+                throw new MyException("haha");
             }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception caught here" + e.ToString());
+            }
+
             finally { Console.WriteLine("akhilesh"); }
+        }
+
+        private static void throwingExceptionprgra()
+        {
+            int x = 9;
+            try
+            {
+                if (x % 0 == 0)
+                    throw new DivideByZeroException("invalid diviion");
+                else
+                    Console.WriteLine("mni hua bc");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
