7c01e8e183a4267c225875bbfa081fcd082bcbcb
Akhilesh Kumar <akhileshsh026@hotmail.com>
2017-04-20 18:45:28 +0530

    Checked unchekced statements checking , these are for checking Airthmatic Overflow checking

---
+++ StringsExpansion/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsExpansion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("\nCHECKED output value is: {0}",
+                              CheckedMethod());
+            Console.WriteLine("UNCHECKED output value is: {0}",
+                              UncheckedMethod());
+
+
+            // Customcehckexample();
+
+        }
+
+        // Set maxIntValue to the maximum value for integers.
+        static int maxIntValue = 2147483647;
+
+        // Using a checked expression.
+        static int CheckedMethod()
+        {
+            int z = 0;
+            try
+            {
+                // The following line raises an exception because it is checked.
+                z = checked(maxIntValue + 10);
+            }
+            catch (System.OverflowException e)
+            {
+                // The following line displays information about the error.
+                Console.WriteLine("CHECKED and CAUGHT:  " + e.ToString());
+            }
+            // The value of z is still 0.
+            return z;
+        }
+
+        // Using an unchecked expression.
+        static int UncheckedMethod()
+        {
+            int z = 0;
+            try
+            {
+                // The following calculation is unchecked and will not 
+                // raise an exception.
+                z = maxIntValue + 10;
+            }
+            catch (System.OverflowException e)
+            {
+                // The following line will not be executed.
+                Console.WriteLine("UNCHECKED and CAUGHT:  " + e.ToString());
+            }
+            // Because of the undetected overflow, the sum of 2147483647 + 10 is 
+            // returned as -2147483639.
+            return z;
+        }
+
+        private static void Customcehckexample()
+        {
+            try
+            {
+                int ten = 27635476;
+                Console.WriteLine(unchecked(2147483647 + ten));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
+
+ 
