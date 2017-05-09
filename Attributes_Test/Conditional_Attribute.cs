a752f69dccb5a82a1f1a40d581034038cc6152dc
Akhilesh Kumar Sahu <Akhileshsh028@outlook.com>
2017-05-09 10:17:03 +0000

    Attributes

---
+++ Attributes_Test/Conditional_Attribute.cs
@@ -0,0 +1,35 @@
+﻿#define DEBUG
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Attributes_Test
+{
+    public class Myclass
+    {
+        [Conditional("DEBUG")]
+        public static void Message(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+     
+    class Conditional_Attribute
+    {
+       public static void fcn1()
+        {
+            Myclass.Message("In function1");
+            fcn2();
+        }
+
+       public  static void fcn2()
+        {
+            Myclass.Message("in Function 2 ");
+        }
+
+    }
+}
---
+++ Attributes_Test/Obsolute_Attribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes_Test
+{
+    class Obsolute_Attribute
+    {
+        public static void Play()
+        {
+            Console.WriteLine("Play the songs from Play method");
+        }
+
+        [Obsolete("Please don't use this method")]
+        public  static void Play1()
+        {
+            Console.WriteLine("Plaging from the obsulte method attribbute");
+        }
+
+        [Obsolete("Please don't use this gives an error",true)]
+        public static void Play2()
+        {
+            Console.WriteLine("implimenting both the perametr of the Obsulte");
+        }
+    }
+}
