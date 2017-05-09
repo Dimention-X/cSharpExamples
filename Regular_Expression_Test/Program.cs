9415fe27911e29e89718d378b2b03a8cc1cd5a11
Akhilesh Kumar Sahu <Akhileshsh028@outlook.com>
2017-05-09 09:10:05 +0000

    Setting theExpressions and web url in the chrome

---
+++ Regular_Expression_Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Regular_Expression_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex reg = new Regex(@"\d+");
+            Match match = reg.Match("Akhilesh");
+            if(match.Success)
+            {
+                Console.WriteLine(match.Value);
+            }
+            else
+                Console.WriteLine("Match nt found");
+        }
+    }
+}
