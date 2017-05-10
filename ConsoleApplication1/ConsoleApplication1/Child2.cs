9d4f51efe4b5d788040f42427fc5b160ece3944e
Akhilesh Kumar <akhileshsh026@users.noreply.github.com>
2017-05-10 17:01:02 +0530

    Reflections At it's peak
    Hoe reflection Works clearly this program mentions.

---
+++ ConsoleApplication1/ConsoleApplication1/Child2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Child2 :Parentt
+    {
+        
+
+        public override void Work1()
+        {
+            Console.WriteLine("Method Work 1 in child 2 class");
+        }
+
+        public override void work2(int id)
+        {
+            Console.WriteLine("Method work 2 in child 2 class with parameter : {0}", id);
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;   // imp 1
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string classname1 = Console.ReadLine();
+            
+            Type objtype = Type.GetType("ConsoleApplication1." + classname1);
+            Console.WriteLine(objtype);
+            object a = Activator.CreateInstance(objtype);
+
+            //Type[] parameter=  new Type[1];
+            //parameter[0] = Type.GetType("System.Int32");
+
+            int id1 = Convert.ToInt32(Console.ReadLine());
+            object[] id = new object[1];
+            id[0] = id1;    
+           
+            //MethodInfo mi = objtype.GetMethod("work2",new Type[1] { Type.GetType("System.Int32")});
+            MethodInfo mi = objtype.GetMethod("DoSomething");
+            mi.Invoke(a, null);
+        }
+    }
+}
