059a44c15cbb1ed4599a8d56d29b295293f39d6a
Akhilesh Kumar <akhileshsh026@users.noreply.github.com>
2017-05-10 17:03:38 +0530

    Reflections in c#
    by using two Child classes and one parent classes we did that .

---
+++ ConsoleApplication1/ConsoleApplication1/Child1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Child1 :Parentt
+    {
+        public override void Work1()
+        {
+            Console.WriteLine("Mthord work 1 in Child1 class");
+        }
+
+        public override void work2(int id)
+        {
+            Console.WriteLine("Method work 2 in child1 classs with patermeter : {0}", id);
+        }
+
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Parentt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Parentt
+    {
+        public void DoSomething()
+        {
+            Work1();
+            work2(23);
+        }
+        
+        public virtual void  Work1()
+        {
+            Console.WriteLine("Calling the simpleprocess method without parameter");
+        }
+
+        public virtual void work2(int id)
+        {
+            Console.WriteLine("Calling the SimpleProcess method with parameter : {0}",id);
+        }
+    }
+}
