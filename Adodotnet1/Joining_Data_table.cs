f825cacc1bb3242ebd48a7b5a1f189f31b329310
Akhilesh Kumar <akhileshsh026@users.noreply.github.com>
2017-05-17 12:39:25 +0530

    Data Table Example
    data table Adding , Deleting , Removing , Modification and Joining the Data Table with example is included in this project.

---
+++ Adodotnet1/Joining_Data_table.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Adodotnet1
+{
+    class Joining_Data_table
+    {
+
+        public static void Alloperations()
+        {
+            DataTable dt1 = new DataTable();
+            dt1.Columns.Add("CustID", typeof(int));
+            dt1.Columns.Add("ColX", typeof(int));
+            dt1.Columns.Add("ColY", typeof(int));
+
+            DataTable dt2 = new DataTable();
+            dt2.Columns.Add("CustID", typeof(int));
+            dt2.Columns.Add("ColZ", typeof(int));
+
+            for (int i = 0; i < 5; i++)
+            {
+                DataRow row1 = dt1.NewRow();
+                row1["CustID"] = i;
+                row1["ColX"] = i + 1;
+                row1["ColY"] = i + 2;
+                dt1.Rows.Add(row1);
+
+                row1 = dt2.NewRow();
+                row1["CustID"] = i;
+                row1["ColZ"] = i + 3;
+                dt2.Rows.Add(row1);
+            }
+
+            var result = from tbl1 in dt1.AsEnumerable()
+                         join
+                             tbl2 in dt2.AsEnumerable() on (int)tbl1["CustID"] equals (int)tbl2["CustID"]
+                         select new
+                         {
+                             CustID = (int)tbl1["CustID"],
+                             ColX = (int)tbl1["ColX"],
+                             ColY = (int)tbl1["ColY"],
+                             ColZ = (int)tbl2["ColZ"]
+                         };
+
+            foreach (var item in result)
+            {
+                Console.WriteLine("ID = {0} , Col1  = {1} , Col2 ={2} , Col3 = {3} ",item.CustID,item.ColX,item.ColY,item.ColZ);
+            }
+        }
+    }
+}
---
+++ Adodotnet1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Adodotnet1
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+
+            //SqlConnection con;
+            //SqlCommand cmd;
+
+            //con = new SqlConnection("User Id=testuser;password=testuser;DataBase=TESTDB;Data Source=bg-in-dspdb1");
+            //cmd = new SqlCommand("Select * from Employee", con);
+            //con.Open();
+            //DataTable dt = new DataTable();
+            //dt.Load(cmd.ExecuteReader());
+
+
+
+            //DataTable dt = GetTable();
+
+          
+            //// datatable Read
+            //foreach (DataRow row in dt.Rows)
+            //{
+            //    Console.WriteLine();
+            //    for (int x = 0; x < dt.Columns.Count; x++)
+            //    {
+            //        Console.Write(row[x].ToString() + " ");
+            //    }
+            //}
+
+            //Console.ReadLine();
+
+            //using (DataTable table = new DataTable())
+            //{
+            //    table.Columns.Add("Name", typeof(string));
+            //    table.Columns.Add(("date"), typeof(DateTime));
+
+            //    table.Rows.Add("Akhilesh", DateTime.Now);
+            //    table.Rows.Add("Dog", DateTime.Today);
+                    
+            //    foreach (DataRow row in table.Rows)
+            //    {
+            //        Console.WriteLine();
+                    
+            //        if (row["Name"].ToString().ToUpper().Equals("DOG"))
+            //        {
+            //            //row["Name"] = "Cortera";       // modifying
+            //            //row["date"] = DateTime.UtcNow;
+            //           // row.Delete();    //deleting 
+            //        }
+                    
+            //        for (int i = 0; i < table.Columns.Count; i++)
+            //        {
+            //            Console.WriteLine(row[i].ToString()+" ");
+            //        }
+            //    }
+            //    table.AcceptChanges();
+            //}
+            //Console.WriteLine(dt.Rows[1].ToString()+" ");
+
+            Joining_Data_table.Alloperations();
+
+            Console.ReadLine();
+           
+        }
+
+          static DataTable GetTable()
+        {    
+              // datatable insertation
+            DataTable table = new DataTable();
+            table.Columns.Add("Eno", typeof(int));
+            table.Columns.Add("Ename", typeof(string));
+            table.Columns.Add("Job", typeof(string));
+
+            table.Rows.Add(1, "Akhielsh", ".NET");
+            table.Rows.Add(2, "Sudeep", ".NET");
+            table.Rows.Add(3, "shetti", ".net");
+            table.Rows.Add(1, "Akhielsh", ".NET");
+            return table;
+        }
+    }
+}
