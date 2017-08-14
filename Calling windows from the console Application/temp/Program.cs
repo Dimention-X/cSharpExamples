21fc14b5ae2262446ec3a1550c14674780876766
Akhilesh Kumar <akhileshsh026@users.noreply.github.com>
2017-08-14 15:56:17 +0530

    Calling the windows form
    calling windows from the   console application where externally inserted the windows form and call it  from the main methord of the console application.

---
+++ Calling windows from the console Application/temp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.IO;
+using Oracle.DataAccess.Client;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+namespace temp
+{
+    class Program
+    {
+        static void Main(string[] args)
+       {
+
+           Application.EnableVisualStyles();
+           Application.Run(new Form1());
+        }
+
+    }
+}
